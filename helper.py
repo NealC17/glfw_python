b04aff13d39c67e11f955d02f4cde4f6a2121d6a
NealC17 <nealxchandra@gmail.com>
2024-08-30 14:35:14 -0700

    gradient triangle

---
+++ helper.py
@@ -0,0 +1,27 @@
+vertex_src = """
+# version 330
+
+in vec3 a_position;
+in vec3 a_color;
+
+out vec3 v_color;
+
+void main()
+{
+    gl_Position = vec4(a_position, 1.0);
+    v_color = a_color;
+}
+"""
+
+fragment_src = """
+# version 330
+
+in vec3 v_color;
+out vec4 out_color;
+
+void main()
+{
+    out_color = vec4(v_color, 1.0);
+}
+"""
+
--- window.py
+++ window.py
@@ -1,5 +1,9 @@
 import glfw
-
+from OpenGL.GL import *
+#from OpenGL.GL import GL_COLOR_BUFFER_BIT, GL_VERTEX_SHADER, GL_FRAGMENT_SHADER
+from OpenGL.GL.shaders import compileProgram, compileShader 
+from helper import vertex_src, fragment_src
+import numpy as np
 
 #start glfw
 if not glfw.init():
@@ -8,7 +12,6 @@ if not glfw.init():
 
 #create the glfw window
 window = glfw.create_window(1280, 720, "title", None, None)
-
 
 
 #window creation failed
@@ -25,9 +28,45 @@ glfw.set_window_pos(window, 400, 200)
 #destroyed when program exits
 glfw.make_context_current(window)
 
+
+vertices = [-0.5, -0.5, 0, 1.0, 0.0, 0.0,
+             0.5, -0.5, 0, 0.0, 1.0, 0.0,
+             0.0,  0.5, 0, 0.0, 0.0, 1.0,]
+
+
+vertices = np.array(vertices, dtype=np.float32)
+vel = np.array([1, 0, 0, 0, 0, 0, 
+                1, 0, 0, 0, 0, 0,
+                1, 0, 0, 0, 0, 0], dtype=np.float32)/100
+
+
+shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER)) #Does the Linking for you so you d o not need to worry about using stuff
+
+
+VBO = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, VBO)
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+position = glGetAttribLocation(shader, "a_position") #has to be the same as the variable in the helper.py file for vertex_src
+
+glEnableVertexAttribArray(position)
+glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+
+color = glGetAttribLocation(shader, "a_color")
+glEnableVertexAttribArray(color)
+glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+
+glUseProgram(shader) #set the binary to the shader
+glClearColor(0, 0, 0.1, 0.1) #Takes (R, G, B, A) 
+#0 0 is the center
 while not glfw.window_should_close(window):
     glfw.poll_events()
+    
+    
+    glClear(GL_COLOR_BUFFER_BIT)
+    glDrawArrays(GL_TRIANGLES, 0, 3)
 
+    #vertices += vel
     glfw.swap_buffers(window)
 
 glfw.terminate()
