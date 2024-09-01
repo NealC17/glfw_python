aed009b4fc79dffe5fc9ff2e158e6ca9f6ae553f
Neal Chandra <nealxchandra@gmail.com>
2024-08-31 18:28:03 -0700

    using taichi

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
 import pygame as pg
 import numpy as np
 import math
-import numba
 import os
-
-os.environ["NUMBA_ENABLE_CUDASIM"] = "1"
+import taichi as ti 
 res = width, height = 800, 450 
 offset = np.array([1.3 * width, height])//2
 max_iter = 30
 zoom = 2.2/height
 texture = pg.image.load('images/texture.jpg')
 texture_size = min(texture.get_size()) - 1
-texture_array = pg.surfarray.array3d(texture)
+texture_array = pg.surfarray.array3d(texture).astype(dtype=np.uint32)
 
 
 
-
+@ti.data_oriented
 class Fractal:
 	def __init__(self, app):
 		self.app = app
 		self.screen_array = np.full((width, height, 3), [0, 0, 0], dtype=np.uint8)
+		ti.init(arch=ti.opengl)
+		self.screen_field = ti.Vector.field(3, ti.uint32, (width, height))
+		self.texture_field=ti.Vector.field(3, ti.uint32, texture.get_size())
+		self.texture_field.from_numpy(texture_array)	
 	
-
-	@staticmethod
-	@numba.njit(fastmath=True, parallel=True)	
-	def render(screen_array):
-		for x in numba.prange(width):
-			for y in range(height):
-				c = (x - offset[0]) * zoom + 1j * (y - offset[1]) * zoom
-				z =0 
-				num_iter =0 
-				
-				for i in range(max_iter):
-					z = z**2 + c
-					if z.real**2 + z.imag**2 > 4:
-						break
-					num_iter +=1 
-				col = int(texture_size* num_iter/max_iter)
-				screen_array[x, y] = texture_array[col, col]
-		return screen_array
+	@ti.kernel
+	def render(self):
+		for x, y in self.screen_field: 
+			c = ti.Vector([(x - offset[0]) * zoom, (y - offset[1]) * zoom])
+			z = ti.Vector([0.0, 0.0])
+			num_iter =0 
+			
+			for i in range(max_iter):
+				z = ti.Vector([(z.x ** 2 - z.y **2 + c.x), (2 * z.x * z.y + c.y)])
+				if z.dot(z)> 4:
+					break
+				num_iter +=1 
+			col = int(texture_size* num_iter/max_iter)
+			self.screen_field[x, y] = self.texture_field[col, col]
 	
 	def update(self):
-		self.screen_array = self.render(self.screen_array)
-		
+		self.render()
+		self.screen_array = self.screen_field.to_numpy()	
 	
 	def draw(self):
 		pg.surfarray.blit_array(self.app.screen, self.screen_array)
