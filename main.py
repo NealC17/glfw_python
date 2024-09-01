f63d7fe06be24f6728fc0b96a2bae31263cbbc65
Neal Chandra <nealxchandra@gmail.com>
2024-08-31 17:57:29 -0700

    Using numpy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import numpy as np
 import math
-
+import numba 
 
 res = width, height = 800, 450 
 offset = np.array([1.3 * width, height])//2
@@ -18,22 +18,23 @@ class Fractal:
 	def __init__(self, app):
 		self.app = app
 		self.screen_array = np.full((width, height, 3), [0, 0, 0], dtype=np.uint8)
-	
+		self.x = np.linspace(0, width, num=width, dtype=np.float32)	
+		self.y = np.linspace(0, height, num=height, dtype=np.float32)	
 	def render(self):
-		for x in range(width):
-			for y in range(height):
-				c = (x - offset[0]) * zoom + 1j * (y - offset[1]) * zoom
-				z =0 
-				num_iter =0 
-				
-				for i in range(max_iter):
-					z = z**2 + c
-					if abs(z) > 2:
-						break
-					num_iter +=1 
-				col = int(texture_size* num_iter/max_iter)
-				self.screen_array[x, y] = texture_array[col, col]
-	
+		x = (self.x - offset[0]) * zoom
+		y = (self.y - offset[1]) * zoom
+		c = x + 1j * y[:, None]
+		
+		num_iter = np.full(c.shape, max_iter)
+		z = np.zeros(c.shape, np.complex128)
+		for i in range(max_iter):
+			mask = (num_iter == max_iter)
+			z[mask] = z[mask] ** 2 + c[mask]
+			z[mask] = np.where(np.abs(z[mask]) > 10, 10 + 0j, z[mask])
+			num_iter[mask & (z.real **2 + z.imag ** 2 > 4.0)] = i+1
+		
+		col = (num_iter.T * texture_size/max_iter).astype(np.uint8)
+		self.screen_array = texture_array[col, col]
 	def update(self):
 		self.render()
 		
