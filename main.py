28a6a768196aedb34d8bf83bca092d58a092aee5
Neal Chandra <nealxchandra@gmail.com>
2024-08-31 18:07:40 -0700

    using numba

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame as pg
 import numpy as np
 import math
+import numba
+import os
 
-
+os.environ["NUMBA_ENABLE_CUDASIM"] = "1"
 res = width, height = 800, 450 
 offset = np.array([1.3 * width, height])//2
 max_iter = 30
@@ -19,8 +21,11 @@ class Fractal:
 		self.app = app
 		self.screen_array = np.full((width, height, 3), [0, 0, 0], dtype=np.uint8)
 	
-	def render(self):
-		for x in range(width):
+
+	@staticmethod
+	@numba.njit(fastmath=True, parallel=True)	
+	def render(screen_array):
+		for x in numba.prange(width):
 			for y in range(height):
 				c = (x - offset[0]) * zoom + 1j * (y - offset[1]) * zoom
 				z =0 
@@ -28,14 +33,15 @@ class Fractal:
 				
 				for i in range(max_iter):
 					z = z**2 + c
-					if abs(z) > 2:
+					if z.real**2 + z.imag**2 > 4:
 						break
 					num_iter +=1 
 				col = int(texture_size* num_iter/max_iter)
-				self.screen_array[x, y] = texture_array[col, col]
+				screen_array[x, y] = texture_array[col, col]
+		return screen_array
 	
 	def update(self):
-		self.render()
+		self.screen_array = self.render(self.screen_array)
 		
 	
 	def draw(self):
